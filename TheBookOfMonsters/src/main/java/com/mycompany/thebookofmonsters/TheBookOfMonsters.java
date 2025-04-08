9e60717698b7e3847c3078aa15da4e411cb2d091
Холостова Арина <airsha2004@gmail.com>
2025-04-08 11:02:10 +0300

    Create Local Repository

---
+++ TheBookOfMonsters/src/main/java/com/mycompany/thebookofmonsters/TheBookOfMonsters.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.thebookofmonsters;
+
+/**
+ *
+ * @author GOSPOGA
+ */
+public class TheBookOfMonsters {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
