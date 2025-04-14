152b2a730f23dbade7cfff59eecc336149357bbe
Холостова Арина <airsha2004@gmail.com>
2025-04-14 13:27:46 +0300

    Delete hello word

--- TheBookOfMonsters/src/main/java/com/mycompany/thebookofmonsters/TheBookOfMonsters.java
+++ TheBookOfMonsters/src/main/java/com/mycompany/thebookofmonsters/TheBookOfMonsters.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 public class TheBookOfMonsters {
 
     public static void main(String[] args) throws IOException, Exception {
-        System.out.println("Тестирование UTF-8 вывода: Привет, мир! 😊");
-
         Controller c = new Controller();
         c.run();
     }
